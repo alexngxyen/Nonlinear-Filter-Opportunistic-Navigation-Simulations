019d2ebb1d95a21c0b77fb9d9c6c51891309273c
alexngxyen <alexaan2@uci.edu>
2022-08-23 22:07:17 -0700

    finalizing EKF

---
+++ Functions/Initialize_Nonlinear_Filters.py
@@ -0,0 +1,216 @@
+import numpy as np
+from scipy import linalg
+
+# Functions
+def initializeSoopVector(n_soops, east_soop_seed=5, north_soop_seed=10):
+    """
+    This function initializes the SoOP's state vector which is composed of the position and clock error states.
+    """
+
+    # Random SoOP Locations
+    np.random.seed(east_soop_seed)
+    east_soop_location = (1500 - (0))*np.random.rand(n_soops, 1) + (0)
+
+    np.random.seed(north_soop_seed)
+    north_soop_location = (500 - (-500))*np.random.rand(n_soops, 1) + (-500)
+
+    # Clock Error States
+    clock_bias  = np.ones((n_soops, 1))
+    clock_drift = 0.1 * np.ones((n_soops, 1))
+
+    # Construct SoOP State Vector
+    soop_state_vector = np.transpose(np.hstack((east_soop_location, north_soop_location, clock_bias, clock_drift)))
+    return soop_state_vector
+
+def clockQuality(clock_type):
+    """
+    This function obtains the power-law coefficients for the clock bias and drift process noise power spectra of a specific clock quality type.
+    """
+
+    if clock_type == ('CSAC'):
+        white_frequency_coefficient       = 2.0e-20
+        frequency_random_walk_coefficient = 4.0e-29   
+    elif clock_type == ('Best OCXO'):
+        white_frequency_coefficient       = 2.6e-22
+        frequency_random_walk_coefficient = 4.0e-26  
+    elif clock_type == ('Typical OCXO'):
+        white_frequency_coefficient       = 8.0e-20
+        frequency_random_walk_coefficient = 4.0e-23 
+    elif clock_type == ('Typical TCXO'):
+        white_frequency_coefficient       = 9.4e-20
+        frequency_random_walk_coefficient = 3.8e-21 
+    elif clock_type == ('Worst TCXO'):
+        white_frequency_coefficient       = 2.0e-19
+        frequency_random_walk_coefficient = 2.0e-20 
+    else:
+        print('clock quality type not recognized.')
+    return white_frequency_coefficient, frequency_random_walk_coefficient
+
+def matrixInitialization(dynamics_state_transition, soop_state_transition, clock_state_transition,
+                         dynamics_process_noise, receiver_clock_process_noise, soop_clock_process_noise, 
+                         receiver_estimation_error_covariance, unknown_soop_estimation_error_covariance, partially_known_soop_estimation_error_covariance, 
+                         measurement_noise, partially_known_soops, unknown_soops):
+    """
+    This function will construct the matrices needed to implement a nonlinear filter.
+    """
+    
+    # Initialize Parameters
+    speed_of_light         = 299792458
+    number_of_measurements = unknown_soops + partially_known_soops
+    number_of_states       = 4 + 2*partially_known_soops + 4*unknown_soops
+    count_partially_known  = 0
+    count_unknown          = 0
+
+    # Linear Transformation Matrix (i.e., Similarity Transformation)
+    linear_transformation_matrix = np.zeros((number_of_states, number_of_states + 2))
+
+    Tr  = np.hstack((np.eye(4), np.zeros((4, 2))))                                     # Transformation Matrix for Receiver's Position/Velocity States
+    Trn = np.tile(np.zeros((4, 2)), (1, partially_known_soops))
+    Trm = np.tile(np.zeros((4, 4)), (1, unknown_soops))
+    linear_transformation_matrix[0:4, :] = np.hstack((Tr, Trn, Trm))
+
+    for i_nz in range(number_of_measurements):                                         # Transformation Matrix for SoOP's Position/Clock Error States
+        if i_nz < partially_known_soops:
+            # Create Transformation Matrix
+            Tsi = -np.eye(2)
+            Tni = np.hstack((np.tile(np.zeros((2, 2)), (1, i_nz)), Tsi, np.tile(np.zeros((2, 2)), (1, partially_known_soops - (i_nz + 1))), 
+                             np.tile(np.zeros((2, 4)), (1, unknown_soops))))
+            Trclk = np.hstack((np.zeros((2, 4)), np.eye(2))) 
+            linear_transformation_matrix[4 + 2*count_partially_known:4 + 2*count_partially_known + 2, :] = np.hstack((Trclk, Tni))
+            
+            # Update Counter
+            count_partially_known += 1    
+
+        elif i_nz >= partially_known_soops and i_nz <= number_of_measurements:
+            # Create Transformation Matrix
+            Tsi = linalg.block_diag(np.eye(2), -np.eye(2)) 
+            Tmi = np.hstack((np.tile(np.zeros((4, 2)), (1, partially_known_soops)), 
+                             np.tile(np.zeros((4, 4)), (i_nz - partially_known_soops)), Tsi, np.tile(np.zeros((4, 4)), (1, number_of_measurements - (i_nz + 1)))))
+            Trclk = linalg.block_diag(np.zeros((2, 4)), np.eye(2))
+            linear_transformation_matrix[4 + 2*partially_known_soops + 4*count_unknown:4 + 2*partially_known_soops + 4*(count_unknown + 1), :] = np.hstack((Trclk, Tmi))
+
+            # Update Counter
+            count_unknown += 1
+            
+        else:
+            print('error')
+
+    # Initialize Matrices
+    Fi = np.empty((0, 0))
+    Pi = np.empty((0, 0))
+    Qi = np.empty((0, 0))
+
+    for i_nz in range(number_of_measurements):                                         
+        if i_nz < partially_known_soops:
+            # State Transition 
+            Fi = linalg.block_diag(Fi, clock_state_transition)
+
+            # Process Covariance 
+            Qi = linalg.block_diag(Qi, speed_of_light**2*soop_clock_process_noise)
+
+            # Estimation Error Covariance
+            Pi = linalg.block_diag(Pi, partially_known_soop_estimation_error_covariance)
+
+        elif i_nz >= partially_known_soops and i_nz <= number_of_measurements:
+            # State Transition 
+            Fi = linalg.block_diag(Fi, soop_state_transition)
+
+            # Process Covariance 
+            Qi = linalg.block_diag(Qi, linalg.block_diag(np.finfo(float).eps*np.eye(2), speed_of_light**2*soop_clock_process_noise))
+
+            # Estimation Error Covariance
+            Pi = linalg.block_diag(Pi, unknown_soop_estimation_error_covariance)
+
+    # State Transition Matrix
+    state_transition_matrix = linalg.block_diag(dynamics_state_transition, Fi)
+    
+    # Input Matrix
+    input_matrix = np.zeros(((len(state_transition_matrix)), 2))
+
+    # Estimation Error Covariance Matrix
+    estimation_error_covariance_matrix = linear_transformation_matrix @ linalg.block_diag(receiver_estimation_error_covariance, Pi) @ linear_transformation_matrix.transpose()
+
+    # Process Covariance Matrix
+    process_covariance_matrix = linear_transformation_matrix @ linalg.block_diag(dynamics_process_noise, speed_of_light**2*receiver_clock_process_noise, Qi) @ linear_transformation_matrix.transpose()
+    
+    # Measurement Covariance Matrix
+    measurement_covariance_matrix = measurement_noise*np.eye(number_of_measurements)
+
+    return linear_transformation_matrix, state_transition_matrix, input_matrix, estimation_error_covariance_matrix, process_covariance_matrix, measurement_covariance_matrix
+
+def constructStateVector(partially_known_soops, unknown_soops, 
+                         receiver_state_vector, soop_state_vector, 
+                         estimation_error_covariance_matrix, linear_transformation_matrix, 
+                         estimate_state_seed=7):
+    """
+    This function will construct the initial true and estimated state vectors for the nonlinear filter.
+    """
+    
+    # Initialize
+    number_of_measurements = partially_known_soops + unknown_soops
+    clock_error_terms      = np.vstack((soop_state_vector[2, :], soop_state_vector[3, :]))
+    
+    # Preallocate
+    ith_soop_state_vector = []
+
+    # Construct SoOP State Vector
+    for i in range(number_of_measurements):
+        if i < partially_known_soops:
+            # i-th SoOP Clock Error State Vector
+            ith_soop_state_vector = np.append(ith_soop_state_vector, clock_error_terms[:, i])
+            
+        elif i >= partially_known_soops and i <= number_of_measurements:
+            # i-th SoOP State Vector
+            ith_soop_state_vector = np.append(ith_soop_state_vector, soop_state_vector[:, i])
+        
+    # True State Vector
+    true_state_vector_untransformed = np.append(receiver_state_vector, ith_soop_state_vector)
+        
+    # Linear Transformation on True State Vector
+    true_state_vector = linear_transformation_matrix @ true_state_vector_untransformed
+
+    # Estimated State Vector
+    np.random.seed(estimate_state_seed)
+    estimate_state_vector = np.random.multivariate_normal(true_state_vector, estimation_error_covariance_matrix).transpose()
+    
+    # Reshape Dimensions
+    true_state_vector     = true_state_vector.reshape((len(true_state_vector), 1))
+    estimate_state_vector = estimate_state_vector.reshape((len(estimate_state_vector), 1))
+
+    # Input State Vector
+    input_state_vector = np.zeros((2, 1))
+    
+    return true_state_vector, estimate_state_vector, input_state_vector
+    
+    
+def truePseudorangeMeasurements(true_state_vector, soop_state_vector, partially_known_soops, unknown_soops):
+    """
+    This function will compute the pseudorange measurements at a specific time instance for the nonlinear filter.
+    """
+    
+    # Initialize
+    number_of_measurements = partially_known_soops + unknown_soops
+    partially_known_index  = (4 + 1) - 1 
+    unknown_index          = (4 + 2*partially_known_soops + 1) - 1
+    
+    # Preallocate
+    pseudorange_measurement = np.zeros((number_of_measurements, 1))
+    
+    for i in range(number_of_measurements):
+        if i < partially_known_soops:
+            # Compute Pseudorange Measurement
+            distance                   = linalg.norm(true_state_vector[0:2] - soop_state_vector[0:2, i].reshape(2, 1))
+            pseudorange_measurement[i] = distance + true_state_vector[partially_known_index]
+            
+            # Update Index
+            partially_known_index += 2
+            
+        elif i >= partially_known_soops and i <= number_of_measurements:
+            # Compute Pseudorange Measurement
+            distance                   = linalg.norm(true_state_vector[0:2] - true_state_vector[unknown_index:unknown_index + 2])
+            pseudorange_measurement[i] = distance + true_state_vector[unknown_index + 2]
+            
+            # Update Index
+            unknown_index += 4
+    
+    return pseudorange_measurement    
--- Navigation_EKF.py
+++ Navigation_EKF.py
@@ -1,163 +1,37 @@
 # Import Packages
+from Functions import Initialize_Nonlinear_Filters
+from Functions import Extended_Kalman_Filter
 import timeit
 import numpy as np
 import matplotlib.pyplot as plt
 import math
 from scipy import linalg
 
-""" Functions """
-# Once the code is complete, create two different .py scripts. One for the common functions between the scripts 
-# and one for the extended kalman filter functions (e.g., time update and measurement update).
-
-def initializeSoopVector(n_soops, east_soop_seed=1, north_soop_seed=2):
-    """
-    This function initializes the SoOP's state vector which is composed of the position and clock error states.
-    """
-
-    # Random SoOP Locations
-    np.random.seed(east_soop_seed)
-    east_soop_location = (1000 - (-250))*np.random.rand(n_soops, 1) + (-250)
-
-    np.random.seed(north_soop_seed)
-    north_soop_location = (500 - (-500))*np.random.rand(n_soops, 1) + (-500)
-
-    # Clock Error States
-    clock_bias  = np.ones((n_soops, 1))
-    clock_drift = 0.1 * np.ones((n_soops, 1))
-
-    # Construct SoOP State Vector
-    xs0 = np.transpose(np.hstack((east_soop_location, north_soop_location, clock_bias, clock_drift)))
-    return xs0
-
-def clockQuality(clock_type):
-    """
-    This function obtains the power-law coefficients for the clock bias and drift process noise power spectra of a specific clock quality type.
-    """
-
-    if clock_type == ('CSAC'):
-        white_frequency_coefficient       = 2.0e-20
-        frequency_random_walk_coefficient = 4.0e-29   
-    elif clock_type == ('Best OCXO'):
-        white_frequency_coefficient       = 2.6e-22
-        frequency_random_walk_coefficient = 4.0e-26  
-    elif clock_type == ('Typical OCXO'):
-        white_frequency_coefficient       = 8.0e-20
-        frequency_random_walk_coefficient = 4.0e-23 
-    elif clock_type == ('Typical TCXO'):
-        white_frequency_coefficient       = 9.4e-20
-        frequency_random_walk_coefficient = 3.8e-21 
-    elif clock_type == ('Worst TCXO'):
-        white_frequency_coefficient       = 2.0e-19
-        frequency_random_walk_coefficient = 2.0e-20 
-    else:
-        print('clock quality type not recognized.')
-    return white_frequency_coefficient, frequency_random_walk_coefficient
-
-def matrixInitialization(dynamics_state_transition, soop_state_transition, clock_state_transition,
-                         dynamics_process_noise, receiver_clock_process_noise, soop_clock_process_noise, 
-                         receiver_estimation_error_covariance, unknown_soop_estimation_error_covariance, partially_known_soop_estimation_error_covariance, 
-                         measurement_noise, partially_known_soops, unknown_soops):
-    """
-    This function will construct the matrices needed to implement the nonlinear filter.
-    """
-    
-    # Initialize Parameters
-    speed_of_light         = 299792458
-    number_of_measurements = unknown_soops + partially_known_soops
-    number_of_states       = 4 + 2*partially_known_soops + 4*unknown_soops
-
-    # Linear Transformation Matrix (i.e., Similarity Transformation)
-    linear_transformation_matrix = np.zeros((number_of_states, number_of_states + 2))
-
-    Tr = np.hstack((np.eye(4), np.zeros((4, 2)) ))                                     # Transformation Matrix for Receiver's Position/Velocity States
-    Trn = np.tile(np.zeros((4, 2)), (1, partially_known_soops))
-    Trm = np.tile(np.zeros((4, 4)), (1, unknown_soops))
-    linear_transformation_matrix[0:4, :] = np.hstack((Tr, Trn, Trm))
-
-    for i_nz in range(number_of_measurements):                                         # Transformation Matrix for SoOP's Position/Clock Error States
-        if i_nz <= partially_known_soops:
-            Tsi = -np.eye(2)
-            Tni = np.hstack((np.tile(np.zeros((2, 2)), (1, i_nz)), Tsi, np.tile(np.zeros((2, 2)), (1, partially_known_soops - (i_nz + 1))), 
-                            np.tile(np.zeros((2, 4)), (1, unknown_soops))))
-            Trclk = np.hstack((np.zeros((2, 4)), np.eye(2))) 
-            linear_transformation_matrix[4 + 2*i_nz:4 + 2*i_nz + 2, :] = np.hstack((Trclk, Tni))    
-
-        elif i_nz > partially_known_soops and i_nz <= number_of_measurements:
-            Tsi = linalg.block_diag(np.eye(2), -np.eye(2)) 
-            Tmi = np.hstack((np.tile(np.zeros((4, 2)), (1, partially_known_soops)), 
-                            np.tile(np.zeros((4, 4)), (1, (i_nz + 1) - (partially_known_soops + 1))), Tsi, np.tile(np.zeros((4, 4)), (1, number_of_measurements - (i_nz + 1)))))
-            Trclk = linalg.block_diag(np.zeros((2, 4)), np.eye(2))
-            linear_transformation_matrix[4 + 2*partially_known_soops + 4*(i_nz + 1):4 + 2*partially_known_soops + 4*(i_nz + 1) + 4, :] = np.hstack((Trclk, Tmi))
-
-        else:
-            print('error')
-
-    # Initialize Matrices
-    Fi = clock_state_transition
-    Pi = partially_known_soop_estimation_error_covariance
-    Qi = c**2*soop_clock_process_noise
-
-    for i_nz in range(number_of_measurements-1):                                         
-        if i_nz <= partially_known_soops:
-            # State Transition 
-            Fi = linalg.block_diag(Fi, clock_state_transition)
-
-            # Process Covariance 
-            Qi = linalg.block_diag(Qi, c**2*soop_clock_process_noise)
-
-            # Estimation Error Covariance
-            Pi = linalg.block_diag(Pi, partially_known_soop_estimation_error_covariance)
-
-        elif i_nz > partially_known_soops and i_nz <= number_of_measurements:
-            # State Transition 
-            Fi = linalg.block_diag(Fi, soop_state_transition)
-
-            # Process Covariance 
-            Qi = linalg.block_diag(Qi, linalg.block_diag(np.finfo(float).eps*np.eye(2), c**2*soop_clock_process_noise))
-
-            # Estimation Error Covariance
-            Pi = linalg.block_diag(Pi,unknown_soop_estimation_error_covariance)
-
-    # State Transition Matrix
-    state_transition_matrix = linalg.block_diag(dynamics_state_transition, Fi)
-    input_matrix = np.zeros(((len(state_transition_matrix)), 2))
-
-    # Estimation Error Covariance Matrix
-    estimation_error_covariance_matrix = linear_transformation_matrix @ linalg.block_diag(receiver_estimation_error_covariance, Pi) @ linear_transformation_matrix.transpose()
-
-    # Process Covariance Matrix
-    process_covariance_matrix = linear_transformation_matrix @ linalg.block_diag(dynamics_process_noise, c**2*receiver_clock_process_noise, Qi) @ linear_transformation_matrix.transpose()
-    
-    # Measurement Covariance Matrix
-    measurement_covariance_matrix = measurement_noise*np.eye(number_of_measurements)
-
-    return linear_transformation_matrix, state_transition_matrix, input_matrix, estimation_error_covariance_matrix, process_covariance_matrix, measurement_covariance_matrix
-
 """ Simulation Parameters """
 # SoOPs
-m  = 0                                                   # Unknown SoOPs
-n  = 10                                                  # Partially-Known (Position States) SoOPs 
-nz = n + m                                               # Total Number of SoOP Measurements
-nx = 4 + 2*n + 4*m                                       # Total Number of States
-
+m  = 0                                                                                # Unknown SoOPs
+n  = 2                                                                               # Partially-Known (Position States) SoOPs 
+nz = n + m                                                                            # Total Number of SoOP Measurements
+nx = 4 + 2*n + 4*m                                                                    # Total Number of States
+ 
 # Time
-c = 299792458                                            # Speed of Light [m/s]
-T = 10e-2                                                # Sampling Period [s]
-t = np.arange(0, 50 + T, T)                              # Experiment Time Duration [s]
-simulation_length = np.size(t)                           # Simulation Time Length [samples]
+c = 299792458                                                                         # Speed of Light [m/s]
+T = 0.1                                                                               # Sampling Period [s]
+t = np.arange(0, 100 + T, T)                                                          # Experiment Time Duration [s]
+simulation_length = np.size(t)                                                        # Simulation Time Length [samples]
 
 # Initialize Receiver Parameters
-x_rx0 = np.array([0, 50, 15, -5, 100, 1]).reshape(6, 1)  # State Vector      
-h0_rx, hneg2_rx = clockQuality('Typical OCXO')           # Typical Oven-Controlled Crystal Oscillator (OCXO)
+x_rx0 = np.array([0, 50, 15, -5, 10, 1]).reshape(6, 1)                                 # State Vector      
+h0_rx, hneg2_rx = Initialize_Nonlinear_Filters.clockQuality('Typical OCXO')           # Typical Oven-Controlled Crystal Oscillator (OCXO)
 
 # Initialize SoOP Parameters
-x_s0 = initializeSoopVector(nz)                          # State Vector                              
-h0_s, hneg2_s = clockQuality('Typical OCXO')             # Typical Oven-Controlled Crystal Oscillator (OCXO) 
+x_s0 = Initialize_Nonlinear_Filters.initializeSoopVector(nz)                          # State Vector                          
+h0_s, hneg2_s = Initialize_Nonlinear_Filters.clockQuality('Typical OCXO')             # Typical Oven-Controlled Crystal Oscillator (OCXO) 
 
 """ Power Spectral Density """
 # Dynamics 
-qx = 0.25                                                # East Position Process Noise
-qy = qx                                                  # North Position Process Noise
+qx = 1.00                                                                             # East Position Process Noise
+qy = qx                                                                               # North Position Process Noise
 
 # Receiver Clock 
 S_wtr    = h0_rx/2
@@ -175,42 +49,105 @@ Fclk = np.array([[1, T], [0, 1]])
 Fs = linalg.block_diag(np.eye(2), Fclk)
 
 # SoOP Process Covariance
-Qclk_s = np.array([[S_wts*T + S_wtsdot*T**3/3, S_wtsdot*T**2/2], [S_wtsdot*T**2/2, S_wtsdot*T]])
+Qclk_s = np.array([[S_wts*T + S_wtsdot*T**3/3, S_wtsdot*T**2/2], 
+                   [S_wtsdot*T**2/2, S_wtsdot*T]])
 
 """ Receiver Dynamics """
 # Velocity Random Walk State Transition Matrix
-Fpv = np.block([[np.eye(2), np.eye(2)*T], [np.zeros((2, 2)), np.eye(2)]])
+Fpv = np.block([[np.eye(2), np.eye(2)*T], 
+                [np.zeros((2, 2)), np.eye(2)]])
 
 # Receiver State Transition Matrix
 Fr = linalg.block_diag(Fpv, Fclk)
 
 # Receiver Process Covariance
-Qpv    = np.array([[qx*T**3/3, 0, qx*T**2/2, 0], [0, qy*T**3/3, 0, qy*T**2/2], [qx*T**2/2, 0, qx*T, 0], [0, qy*T**2/2, 0, qy*T]])
-Qclk_r = np.array([[S_wtr*T + S_wtrdot*T**3/3, S_wtrdot*T**2/2], [S_wtrdot*T**2/2, S_wtrdot*T]])
+Qpv    = np.array([[qx*T**3/3, 0, qx*T**2/2, 0], [0, qy*T**3/3, 0, qy*T**2/2], 
+                   [qx*T**2/2, 0, qx*T, 0], [0, qy*T**2/2, 0, qy*T]])
+Qclk_r = np.array([[S_wtr*T + S_wtrdot*T**3/3, S_wtrdot*T**2/2], 
+                   [S_wtrdot*T**2/2, S_wtrdot*T]])
 
 """ Initialize Nonlinear Filter"""
 # Nonlinear Filter Parameters
 measurement_noise = 25                            
-P_rx0  = linalg.block_diag(5**2*np.eye(2), 1**2*np.eye(2), 30**2, 0.3**2)                # Receiver's Initial Estimation Error Covariance Matrix
-P_s0   = linalg.block_diag(1e3*np.eye(2), 30**2, 0.3**2)                                 # Unknown SoOP's Initial Estimation Error Covariance Matrix
-P_clk0 = linalg.block_diag(30**2, 0.3**2)                                                # Partially-Known SoOP's Initial Estimation Error Covariance Matrix
+P_rx0  = linalg.block_diag(5**2*np.eye(2), 1**2*np.eye(2), 300**2, 3**2)                 # Receiver's Initial Estimation Error Covariance Matrix
+P_s0   = linalg.block_diag(1e3*np.eye(2), 300**2, 3**2)                                  # Unknown SoOP's Initial Estimation Error Covariance Matrix
+P_clk0 = linalg.block_diag(300**2, 3**2)                                                 # Partially-Known SoOP's Initial Estimation Error Covariance Matrix
 
 # Construct Necessary Matrices (e.g., T, F, G, P, Q, R)
-T, F, G, P, Q, R = matrixInitialization(Fpv, Fs, Fclk, Qpv, Qclk_r, Qclk_s, P_rx0, P_s0, P_clk0, measurement_noise, n, m)
+LT, F, G, P, Q, R = Initialize_Nonlinear_Filters.matrixInitialization(Fpv, Fs, Fclk, Qpv, Qclk_r, Qclk_s, P_rx0, P_s0, P_clk0, measurement_noise, n, m)
 
 # Process and Measurement Noise
 q = linalg.cholesky(Q, lower=True)
 r = linalg.cholesky(R, lower=True) 
 
 # Construct Extended Kalman Filter State Vector
+P_est = P
+x_true, x_est, u = Initialize_Nonlinear_Filters.constructStateVector(n, m, x_rx0, x_s0, P_est, LT)
 
+""" Extended Kalman Filter """
+# Preallocation
+x_true_hist = np.zeros((nx, simulation_length))
+x_est_hist  = np.zeros((nx, simulation_length))
+P_std_hist  = np.zeros((nx, simulation_length))
+zk_hist     = np.zeros((nz, simulation_length))
+zk_hat_hist = np.zeros((nz, simulation_length)) 
 
-# Extended Kalman Filter
-start = timeit.default_timer()                                                            # Start Timer
+# Start Timer
+start = timeit.default_timer()    
 
+for k in range(simulation_length):       
+    # Set Random Noise Seed
+    np.random.seed(k)
+    wk = q @ np.random.randn(nx, 1)  
+    np.random.seed(k)                                            
+    vk = r @ np.random.randn(nz, 1)
+    
+    # True Pseudorange Measurements 
+    h_zk = Initialize_Nonlinear_Filters.truePseudorangeMeasurements(x_true, x_s0, n, m)
+    zk   = h_zk + vk
+    
+    # Prediction Step
+    x_predict, P_predict = Extended_Kalman_Filter.predictionStep(x_est, u, F, G, Q, P_est)
 
-""" Input Extended Kalman filter code here (e.g., x_hat, P_hat, x_hat_new, P_hat_new) - maybe use functions"""
+    # Estimate Pseudorange Measurements
+    zk_hat, H = Extended_Kalman_Filter.estimatedPseudorangeMeasurements(x_predict, x_s0, n, m)
 
+    # Correction Step
+    x_correct, P_correct = Extended_Kalman_Filter.correctionStep(x_predict, P_predict, zk, zk_hat, H, R)    
 
-end = timeit.default_timer()                                                              # End Timer
+    # Save Values
+    x_true_hist[:, k:k+1] = x_true
+    x_est_hist[:, k:k+1]  = x_correct
+    P_std_hist[:, k:k+1]  = np.sqrt(P_correct.diagonal()).reshape(nx, 1)
+    zk_hist[:, k:k+1]     = zk
+    zk_hat_hist[:, k:k+1] = zk_hat
+    
+    # Update State Space 
+    x_true = F @ x_true + G @ u + wk
+    x_est  = x_correct
+    P_est  = P_correct
+    
+# End Timer
+end = timeit.default_timer()                                                              
+print("Elapsed Time for EKF:", end - start, "seconds")
 
+# Estimation Error Trajectories
+x_tilde_hist = x_true_hist - x_est_hist
+
+# Delete
+RMSE = np.sqrt(np.mean(x_tilde_hist**2, axis=1))
+pos_RMSE = linalg.norm(sum(RMSE[0:2]))
+vel_RMSE = linalg.norm(sum(RMSE[2:4]))
+print("Position RMSE", pos_RMSE)
+print("Velocity RMSE", vel_RMSE)
+
+plt.figure()
+plt.plot(x_true_hist[0,:], x_true_hist[1,:])
+plt.plot(x_est_hist[0,:], x_est_hist[1,:])
+plt.scatter(x_s0[0,:],x_s0[1,:], color = 'red')
+
+plt.figure()
+for a in range(nz):
+    plt.plot(t, zk_hist[a, :] - zk_hat_hist[a, :])
+
+plt.show()
